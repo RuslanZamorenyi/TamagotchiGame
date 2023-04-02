f118fbb59a4998a1c320e95252d0883cd5e7bf01
Ruslan <rzamorenyj@gmail.com>
2023-04-02 20:37:42 +0300

    created

--- main.py
+++ main.py
@@ -30,67 +30,67 @@ class Animal:
             print(f"Your parameters are:\nfood= {self.food},\twater= {self.water},\tentertainment= {self.entertainment}"
                   f",\tcheerful= {self.sleep},\thealth= {self.health}\nEach max value is 100")
 
-    def start_play(self):
+    def level_food(self):
+        self.food += 10
+        if self.food >= 100:
+            self.health -= 30
+            if self.health <= 0:
+                sys.exit("Health is over, your animal died")
+        elif self.food <= 0:
+            self.health -= 40
+            print("Your levels food=", self.food, "and health=", self.health)
+        return self.food, self.health
 
+    def level_water(self):
+        self.water += 10
+        if self.water >= 100:
+            self.health -= 30
+            if self.health <= 0:
+                sys.exit("Health is over, your animal died")
+        elif self.water <= 0:
+            self.health -= 40
+        print("Your levels water=", self.water, "and health=", self.health)
+        return self.water, self.health
+
+    def level_entertainment(self):
+        self.entertainment += 10
+        if self.entertainment >= 100:
+            self.health -= 30
+            if self.health <= 0:
+                sys.exit("Health is over, your animal died")
+        elif self.entertainment <= 0:
+            self.health -= 40
+        print("Your levels funny=", self.entertainment, "and health=", self.health)
+        return self.entertainment, self.health
+
+    def level_sleep(self):
+        self.sleep += 10
+        if self.sleep >= 100:
+            self.health -= 30
+            if self.health <= 0:
+                sys.exit("Health is over, your animal died")
+        elif self.sleep <= 0:
+            self.health -= 40
+        print("Your levels tired=", self.sleep, "and health=", self.health)
+        return self.water, self.health
+
+    def start_play(self):
         while True:
             print(f"Your parameters are:\nfood= {self.food},\twater= {self.water},\tentertainment= {self.entertainment}"
                   f",\tcheerful= {self.sleep},\thealth= {self.health}\nEach max value is 100")
 
-            def level_food(self):
-                self.food += 10
-                if self.food >= 100:
-                    self.health -= 30
-                    if self.health <= 0:
-                        sys.exit("Health is over, your animal died")
-                elif self.food <= 0:
-                    self.health -= 40
-                    print("Your levels food=", self.food, "and health=", self.health)
-                return self.food, self.health
-
-            def level_water(self):
-                self.water += 10
-                if self.water >= 100:
-                    self.health -= 30
-                    if self.health <= 0:
-                        sys.exit("Health is over, your animal died")
-                elif self.water <= 0:
-                    self.health -= 40
-                print("Your levels water=", self.water, "and health=", self.health)
-                return self.water, self.health
-
-            def level_entertainment(self):
-                self.entertainment += 10
-                if self.entertainment >= 100:
-                    self.health -= 30
-                    if self.health <= 0:
-                        sys.exit("Health is over, your animal died")
-                elif self.entertainment <= 0:
-                    self.health -= 40
-                print("Your levels funny=", self.entertainment, "and health=", self.health)
-                return self.entertainment, self.health
-
-            def level_sleep(self):
-                self.sleep += 10
-                if self.sleep >= 100:
-                    self.health -= 30
-                    if self.health <= 0:
-                        sys.exit("Health is over, your animal died")
-                elif self.sleep <= 0:
-                    self.health -= 40
-                print("Your levels tired=", self.sleep, "and health=", self.health)
-                return self.water, self.health
             answer = input("Hello, what do you want to do with your pet\n\tfeed\n\tgive to drink\n\tentertain\n\tsleep"
                            "\nFor exit write exit\nPlease write here:").lower()
 
             match answer:
                 case "feed":
-                    level_food(self)
+                    Animal.level_food(self)
                 case "give to drink":
-                    level_water(self)
+                    Animal.level_water(self)
                 case "entertain":
-                    level_entertainment(self)
+                    Animal.level_entertainment(self)
                 case "sleep":
-                    level_sleep(self)
+                    Animal.level_sleep(self)
                 case "exit":
                     break
                 case _:
